a56be04f4a785c836c49c777af6c2012c30f1e2a
Rocco Jiang <rocco@roccojiang.com>
2024-04-17 23:28:25 +0100

    Implement basic unfold algorithm

--- parsley-garnish/rules/src/main/scala/fix/FactorLeftRecursion.scala
+++ parsley-garnish/rules/src/main/scala/fix/FactorLeftRecursion.scala
@@ -1,12 +1,15 @@
 package fix
 
 import metaconfig.Configured
+import scala.collection.mutable
 import scala.meta._
 import scalafix.v1._
 import scalafix.lint.LintSeverity
 
 import utils.NonTerminalDetection.{NonTerminalTree, getNonTerminals}
-import utils.ParserWithTerm
+import utils.Matchers
+import utils.Parser, utils.Parser._
+// import utils.ParserWithTerm
 
 case class FactorLeftRecursionConfig(debugOptions: List[String] = List.empty) {
   def reportNonTerminalLocations: Boolean = debugOptions.contains("reportNonTerminalLocations")
@@ -30,8 +33,12 @@ class FactorLeftRecursion(config: FactorLeftRecursionConfig) extends SemanticRul
   override def fix(implicit doc: SemanticDocument): Patch = {
     implicit val env = getNonTerminals
     val nonTerminals = env.map {
-      case (sym, NonTerminalTree(_, body, _)) =>
-        sym -> ParserWithTerm(body)
+      case (nonTerminalSymbol, NonTerminalTree(_, bodyTerm, _)) =>
+        nonTerminalSymbol -> Parser(bodyTerm)
+    }.to(mutable.Map)
+
+    for ((sym, parser) <- nonTerminals) {
+      nonTerminals(sym) = transform(unfold(nonTerminals, sym))
     }
 
     pprint.pprintln(nonTerminals)
@@ -43,12 +50,104 @@ class FactorLeftRecursion(config: FactorLeftRecursionConfig) extends SemanticRul
     }
   }
 
+  private def transform(unfolded: UnfoldedProduction): Parser = {
+    val UnfoldedProduction(empty, nonLeftRec, leftRec) = unfolded
+    val empties = empty match {
+      case None    => Empty
+      case Some(t) => Pure(t)
+    }
+
+    Postfix(nonLeftRec <|> empties, leftRec)
+  }
+
+  private def unfold(env: mutable.Map[Symbol, Parser], nonTerminal: Symbol)(implicit doc: SemanticDocument): UnfoldedProduction = {
+    def unfold0(visited: Set[Symbol], nt: Parser): UnfoldedProduction = nt match {
+      case p @ NonTerminal(sym) => {
+        println(s"found a non-terminal: ${nt.term}")
+        println(s"\tvisited = $visited")
+
+        val tpe = utils.getType(sym.info.get.signature).collect {
+          case TypeRef(_, Matchers.parsley(_), List(t)) => Type.Name(t.toString)
+        }
+        assert(tpe.isDefined, s"expected a Parsley type for $sym, got ${sym.info.get.signature}")
+
+        if (sym == nonTerminal) {
+          println(s"\tmatched non-terminal $sym")
+          // TODO: is the type used here actually correct or am I just making this up?
+          UnfoldedProduction(None, Empty, Pure(q"identity[${tpe.get}] _"))
+        } else if (visited.contains(sym)) {
+          println(s"\talready visited $sym")
+          UnfoldedProduction(None, p, Empty)
+        } else {
+          println(s"\tvisiting $sym")
+          unfold0(visited + sym, env(sym))
+        }
+      }
+
+      case p @ Str(_) => UnfoldedProduction(None, p, Empty)
+      case Pure(x) => UnfoldedProduction(Some(x), Empty, Empty)
+      case Empty => UnfoldedProduction(None, Empty, Empty)
+
+      case FMap(p, f) => unfold0(visited, Pure(f) <*> p)
+
+      case Or(p, q) => {
+        val UnfoldedProduction(pe, pn, pl) = unfold0(visited, p)
+        val UnfoldedProduction(qe, qn, ql) = unfold0(visited, q)
+
+        // TODO: originally in the paper this was pe.xor(qe), but I think that's not true under PEG semantics?
+        UnfoldedProduction(pe.orElse(qe), pn <|> qn, pl <|> ql)
+      }
+      
+      case r @ Ap(p, q) => {
+        val UnfoldedProduction(pe, pn, pl) = unfold0(visited, p)
+        val UnfoldedProduction(qe, qn, ql) = unfold0(visited, q)
+
+        val empty = if (pe.isDefined && qe.isDefined) {
+          // TODO: does this work as an implementation of the original bothEmpty? does it assume currying?
+          Some(q"${pe.get}(${qe.get})") // pure f <*> pure x = pure (f x)
+        } else {
+          None
+        }
+
+        val lefts = {
+          // TODO: implement flipping
+          val llr = pl.map(q"flip(_)") <*> q
+          val rlr = pe match {
+            case None    => Empty
+            case Some(f) => ql.map(q"$f.compose(_)")
+          }
+          
+          llr <|> rlr
+        }
+
+        val nonLefts = {
+          val lnl = pn <*> q
+          val rnl = pe match {
+            case None =>    Empty
+            case Some(f) => qn.map(f)
+          }
+          
+          lnl <|> rnl
+        }
+
+        UnfoldedProduction(empty, nonLefts, lefts)
+      }
+
+      // TODO
+      case unhandled => UnfoldedProduction(None, unhandled, Empty)
+    }
+
+    unfold0(Set.empty, env(nonTerminal))
+  }
+
   private def lintNonTerminalLocations(implicit doc: SemanticDocument): Patch = {
     getNonTerminals.map {
       case (_, NonTerminalTree(name, _, originalTree)) => Patch.lint(NonTerminalLint(originalTree, name.value))
     }.asPatch
   }
 }
+
+case class UnfoldedProduction(empty: Option[Term], nonLeftRec: Parser, leftRec: Parser)
 
 case class NonTerminalLint(defn: Defn, name: String) extends Diagnostic {
   override def position: Position = defn.pos
--- parsley-garnish/rules/src/main/scala/fix/utils/Parser.scala
+++ parsley-garnish/rules/src/main/scala/fix/utils/Parser.scala
@@ -5,54 +5,162 @@ import scalafix.v1._
 
 import NonTerminalDetection.NonTerminalTree
 
-sealed abstract class Parser extends Product with Serializable
-object Parser {
-  // TODO: for everything that uses Term arguments, should we use a different type? Perhaps some way to represent functions, for defunctionalisation?
-  final case class NonTerminal(ref: Symbol) extends Parser
-  final case class Pure(a: Term) extends Parser
-  final case object Empty extends Parser
-  /* p: Parser[A], q: Parser[A] */
-  final case class Or(p: Parser, q: Parser) extends Parser
-  // TODO: generalise to lift2?
-  /* p: Parser[A => B], q: Parser[A] */
-  final case class <*>(p: Parser, q: Parser) extends Parser
-  /* p: Parser[A], f: A => B */
-  final case class FMap(p: Parser, f: Term) extends Parser
-  final case class Many(p: Parser) extends Parser
-  final case class Str(str: String) extends Parser
-
-  final case object Unknown extends Parser
+sealed abstract class Parser extends Product with Serializable {
+  def term: Term
 }
+object Parser {
+  final case class NonTerminal(val ref: Symbol)(implicit doc: SemanticDocument) extends Parser {
+    val term = Term.Name(ref.info.get.displayName) // TODO: I think this is correct, but needs checking
+  }
 
-// The original term this parser is connected to. If None, then this parser is a result of a transformation
-// TODO: better naming?
-case class ParserWithTerm(parser: Parser, term: Term)
-object ParserWithTerm {
-  import Parser._
+  final case class Pure(x: Term) extends Parser {
+    val term = q"pure($x)"
+  }
 
-  def apply(term: Term)(implicit env: Map[Symbol, NonTerminalTree], doc: SemanticDocument): ParserWithTerm = {
-    val parser = term match {
-      // See https://scalacenter.github.io/scalafix/docs/developers/symbol-matcher.html#unapplytree for how to mitigate
-      // against matching multiple times using SymbolMatchers
+  final case object Empty extends Parser {
+    val term = Term.Name("empty")
+  }
 
-      case t if env.contains(t.symbol) => NonTerminal(t.symbol)
+  final case class Or(p: Parser, q: Parser) extends Parser {
+    // TODO: think about this
+    // Note: this doesn't preserve the original combinator choice (i.e. <|> or |) but it doesn't really matter imo
+    val term = q"${p.term} | ${q.term}"
+  }
 
-      // "string(s)"
-      case Term.Apply.After_4_6_0(Matchers.string(_), Term.ArgClause(List(s), _)) => Str(s.toString)
-      // "empty"
-      case Matchers.empty(Term.Name(_)) => Empty
-      // "pure(x)"
-      case Term.Apply.After_4_6_0(Matchers.pure(_), Term.ArgClause(List(x), _)) => Pure(x)
-      // "p.map(f)"
-      case Term.Apply.After_4_6_0(Term.Select(qual, Matchers.map(_)), Term.ArgClause(List(f), _)) => FMap(apply(qual).parser, f)
-      // "p <|> q" or "p | q"
-      case Term.ApplyInfix.After_4_6_0(p, Matchers.<|>(_), _, Term.ArgClause(List(q), _)) => Or(apply(p).parser, apply(q).parser)
-      // "p <*> q"
-      case Term.ApplyInfix.After_4_6_0(p, Matchers.<*>(_), _, Term.ArgClause(List(q), _)) => <*>(apply(p).parser, apply(q).parser)
+  // TODO: generalise to lift2?
+  // final case class Ap[A, B](p: Parser, q: Parser) extends Parser {
+  final case class Ap(p: Parser, q: Parser) extends Parser {
+    val term = q"${p.term} <*> ${q.term}"
+  }
 
-      case _ => Unknown
+  final case class FMap(p: Parser, f: Term) extends Parser {
+    val term = q"${p.term}.map($f)"
+  }
+
+  final case class Many(p: Parser) extends Parser {
+    val term = q"many(${p.term})"
+  }
+
+  final case class Str(s: String) extends Parser {
+    val term = q"string($s)"
+  }
+
+  final case class Postfix(p: Parser, op: Parser) extends Parser {
+    val term = q"chain.postfix(${p.term})(${op.term})"
+  }
+
+  final case class Unknown(unrecognisedTerm: Term) extends Parser {
+    val term = unrecognisedTerm
+  }
+
+  def apply(term: Term)(implicit env: Map[Symbol, NonTerminalTree], doc: SemanticDocument): Parser = term match {
+    // See https://scalacenter.github.io/scalafix/docs/developers/symbol-matcher.html#unapplytree for how to mitigate
+    // against matching multiple times using SymbolMatchers
+
+    case t if env.contains(t.symbol) => NonTerminal(t.symbol)
+
+    // "string(s)"
+    case Term.Apply.After_4_6_0(Matchers.string(_), Term.ArgClause(List(s), _)) => Str(s.toString)
+    // "empty"
+    case Matchers.empty(Term.Name(_)) => Empty
+    // "pure(x)"
+    case Term.Apply.After_4_6_0(Matchers.pure(_), Term.ArgClause(List(x), _)) => Pure(x)
+    // "p.map(f)"
+    case Term.Apply.After_4_6_0(Term.Select(qual, Matchers.map(_)), Term.ArgClause(List(f), _)) => FMap(apply(qual), f)
+    // "p <|> q" or "p | q"
+    case Term.ApplyInfix.After_4_6_0(p, Matchers.<|>(_), _, Term.ArgClause(List(q), _)) => Or(apply(p), apply(q))
+    // "p <*> q"
+    case Term.ApplyInfix.After_4_6_0(p, Matchers.<*>(_), _, Term.ArgClause(List(q), _)) => Ap(apply(p), apply(q))
+
+    case unrecognisedTerm => Unknown(unrecognisedTerm)
+  }
+
+  implicit class ParserOps(p: Parser) {
+    def <|>(q: Parser): Parser = (p, q) match {
+      case (p, Empty)   => p
+      case (Empty, q)   => q
+      case (Pure(_), _) => p
+      case (p, q)       => Or(p, q)
     }
 
-    ParserWithTerm(parser, term)
+    def <*>(q: Parser): Parser = (p, q) match {
+      case (Empty, _) => Empty
+      case (p, q)     => Ap(p, q)
+    }
+
+    def map(f: Term): Parser = (p, f) match {
+      // case (Empty, _) => Empty // TODO: does this hold?
+      case (p, f)     => FMap(p, f)
+    }
   }
 }
+
+
+// sealed abstract class Parser extends Product with Serializable
+// object Parser {
+//   // TODO: for everything that uses Term arguments, should we use a different type? Perhaps some way to represent functions, for defunctionalisation?
+//   final case class NonTerminal(ref: Symbol) extends Parser
+//   final case class Pure(a: Term) extends Parser
+//   final case object Empty extends Parser
+//   /* p: Parser[A], q: Parser[A] */
+//   final case class Or(p: Parser, q: Parser) extends Parser
+//   // TODO: generalise to lift2?
+//   /* p: Parser[A => B], q: Parser[A] */
+//   final case class <*>(p: Parser, q: Parser) extends Parser
+//   /* p: Parser[A], f: A => B */
+//   final case class FMap(p: Parser, f: Term) extends Parser
+//   final case class Many(p: Parser) extends Parser
+//   final case class Str(str: String) extends Parser
+
+//   final case object Unknown extends Parser
+// }
+
+// // The original term this parser is connected to. If None, then this parser is a result of a transformation
+// // TODO: better naming?
+// sealed abstract class ParserWithTerm(val parser: Parser, val term: Term) extends Product with Serializable
+// // sealed abstract class ParserWithTerm extends Product with Serializable {
+// //   def parser: Parser
+// //   def term: Term
+// // }
+// object ParserWithTerm {
+//   import Parser._
+
+//   // TODO: this still sucks ugh
+//   // final case class NonTerminal(ref: Symbol)(implicit doc: SemanticDocument) extends ParserWithTerm {
+//   //   val parser = Parser.NonTerminal(ref)
+//   //   val term = Term.Name(ref.info.get.displayName) // TODO: I think this is correct, but needs checking
+//   // }
+
+//   final case class NonTerminal(parser: Parser.NonTerminal, term: Term) extends ParserWithTerm(parser, term)
+//   object NonTerminal {
+//     def apply(ref: Symbol)(implicit doc: SemanticDocument): NonTerminal = {
+//       NonTerminal(Parser.NonTerminal(ref), Term.Name(ref.info.get.displayName))
+//     }
+//   }
+
+//   def apply(term: Term)(implicit env: Map[Symbol, NonTerminalTree], doc: SemanticDocument): ParserWithTerm = {
+//     val parser = term match {
+//       // See https://scalacenter.github.io/scalafix/docs/developers/symbol-matcher.html#unapplytree for how to mitigate
+//       // against matching multiple times using SymbolMatchers
+
+//       case t if env.contains(t.symbol) => NonTerminal(t.symbol)
+
+//       // "string(s)"
+//       case Term.Apply.After_4_6_0(Matchers.string(_), Term.ArgClause(List(s), _)) => Str(s.toString)
+//       // "empty"
+//       case Matchers.empty(Term.Name(_)) => Empty
+//       // "pure(x)"
+//       case Term.Apply.After_4_6_0(Matchers.pure(_), Term.ArgClause(List(x), _)) => Pure(x)
+//       // "p.map(f)"
+//       case Term.Apply.After_4_6_0(Term.Select(qual, Matchers.map(_)), Term.ArgClause(List(f), _)) => FMap(apply(qual).parser, f)
+//       // "p <|> q" or "p | q"
+//       case Term.ApplyInfix.After_4_6_0(p, Matchers.<|>(_), _, Term.ArgClause(List(q), _)) => Or(apply(p).parser, apply(q).parser)
+//       // "p <*> q"
+//       case Term.ApplyInfix.After_4_6_0(p, Matchers.<*>(_), _, Term.ArgClause(List(q), _)) => <*>(apply(p).parser, apply(q).parser)
+
+//       case _ => Unknown
+//     }
+
+//     ParserWithTerm(parser, term)
+//   }
+// }
