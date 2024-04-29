e1bc71da3c2610bf6ff6e74788e023b298356dbd
Rocco Jiang <rocco@roccojiang.com>
2024-04-29 19:38:39 +0100

    Add type parameters to parsers and funcs

--- parsley-garnish/rules/src/main/scala/fix/utils/Func.scala
+++ parsley-garnish/rules/src/main/scala/fix/utils/Func.scala
@@ -2,53 +2,62 @@ package fix.utils
 
 import scala.meta._
 
-// sealed abstract class Func[A] extends Product with Serializable {
-sealed abstract class Func extends Product with Serializable {
+sealed abstract class Func[+A] extends Product with Serializable {
   import Func._
 
   def term: Term
 
-  def simplify: Func = this match {
-    // identity(x) = x
-    case App(Id(_), x) => x.simplify
+  def simplify = ???
 
-    // f.compose(identity) = f
-    case App(App(Compose, f), Id(_)) => f.simplify
-    // identity.compose(f) = f
-    case App(App(Compose, Id(_)), f) => f.simplify
+  // def simplify: Func = this match {
+  //   // case App(Abs(x, f), y) => f.substitute(x, y).simplify
 
-    case App(f, x) => App(f.simplify, x.simplify)
+  //   // identity(x) = x
+  //   case App(Id(_), x) => x.simplify
 
-    case _ => this
-  }
+  //   // f.compose(identity) = f
+  //   case App(App(Compose, f), Id(_)) => f.simplify
+  //   // identity.compose(f) = f
+  //   case App(App(Compose, Id(_)), f) => f.simplify
+
+  //   case App(f, x) => App(f.simplify, x.simplify)
+
+  //   case _ => this
+  // }
 }
 
 object Func {
-  case class Abs(f: Func) extends Func {
-    val term = q"${f.term}(_)"
+  case class Abs[A, B](x: A, f: Func[B]) extends Func[A => B] {
+    // val term = q"($x) => ${f.term}"
+    val term = ???
+
+    // val test = (a: Term.Param) => (b) => Abs(a, b)
   }
 
-  case class App(f: Func, x: Func) extends Func {
-    val term = f match {
-      case Compose => q"${x.term}.compose"
-      case _ => q"${f.term}(${x.term})"
-    }
+  case class App[A, B](f: Func[A => B], x: Func[A]) extends Func[B] {
+    // val term = f match {
+    //   case Compose => q"${x.term}.compose"
+    //   case _ => q"${f.term}(${x.term})"
+    // }
+    val term = ???
   }
 
 
-  case class Id(tpe: Type) extends Func {
+  case class Id[T <: Type](tpe: T) extends Func[T => T] {
     val term = q"identity[$tpe]"
+
+    val t: Term = Term.fresh()
   }
 
-  case object Compose extends Func {
+  case class Compose[A, B, C]() extends Func[(B => C) => (A => B) => (A => C)] {
     val term = q"compose"
   }
 
-  case object Flip extends Func {
+  case class Flip[A, B, C]() extends Func[(A => B => C) => (B => A => C)] {
     val term = q"flip"
   }
 
-  case class Opaque(t: Term, shouldCurry: Boolean) extends Func {
+  case class Opaque[A](t: Term, shouldCurry: Boolean) extends Func[A] {
     val term = if (shouldCurry) p"$t.curried" else t
   }
 
@@ -62,5 +71,20 @@ object Func {
 
   // case class Compose[A, B, C]() extends Func[(B => C) => (A => B) => A => C] {
   //   val term = q"compose"
+  // }
+
+  def flip[A, B, C](f: Func[A => B => C]): Func[B => A => C] = App(Flip[A, B, C](), f)
+
+  def compose[A, B, C](f: Func[B => C])(g: Func[A => B]): Func[A => C] = App(App(Compose[A, B, C](), f), g)
+
+
+  val test = compose(Id[Type](Type.Name("Int")))
+  val test2 = flip
+
+  // implicit class FuncOps[A, B, C](f: Func[B => C]) {
+  //   def compose(g: Func[A => B]): Func[A => C] = App(App(Compose[A, B, C](), f), g)
+  // }
+  // implicit class FuncOps2[A, B, C](f: Func[A => B => C]) {
+  //   def flip(): Func[B => A => C] = App(Flip(), f)
   // }
 }
--- parsley-garnish/rules/src/main/scala/fix/utils/Parser.scala
+++ parsley-garnish/rules/src/main/scala/fix/utils/Parser.scala
@@ -6,45 +6,52 @@ import scalafix.v1._
 import Func._
 import NonTerminalDetection.NonTerminalTree
 
-sealed abstract class Parser extends Product with Serializable {
+sealed abstract class Parser[+A] extends Product with Serializable {
   import Parser._
   
   def term: Term
 
-  def simplify: Parser = this match {
-    case Choice(p, Empty)   => p.simplify
-    case Choice(Empty, q)   => q.simplify
-    case Choice(Pure(f), _) => Pure(f.simplify)
-    case Choice(p, q)       => Choice(p.simplify, q.simplify)
+  def simplify: Parser[A] = {
 
-    case Ap(Empty, _)         => Empty
-    case Ap(Pure(f), Pure(x)) => Pure(App(f.simplify, x.simplify).simplify)
-    case Ap(Pure(f), x)       => FMap(x.simplify, f.simplify)
-    case Ap(p, q)             => Ap(p.simplify, q.simplify)
+    def simplifyMap[B, C](p: Parser[B], f: Func[B => C]): Parser[C] = (p, f) match {
+      case (Empty, _) => Empty // TODO: does this hold?
+      // case (p, Id(_)) => p.simplify
+      case (Pure(x), f) => Pure(App(f.simplify, x.simplify).simplify)
+      case (p, f) => FMap(p.simplify, f.simplify)
+    }
 
-    case FMap(Empty, _)   => Empty // TODO: does this hold?
-    case FMap(p, Id(_))   => p.simplify
-    case FMap(Pure(x), f) => Pure(App(f.simplify, x.simplify).simplify)
-    case FMap(p, f)       => FMap(p.simplify, f.simplify)
+    this match {
+      case Choice(p, Empty)   => p.simplify
+      case Choice(Empty, q)   => q.simplify
+      case Choice(Pure(f), _) => Pure(f.simplify)
+      case Choice(p, q)       => Choice(p.simplify, q.simplify)
 
-    case Many(Empty)      => Empty
-    case Many(p)          => Many(p.simplify)
+      case Ap(Empty, _)         => Empty
+      case Ap(Pure(f), Pure(x)) => Pure(App(f.simplify, x.simplify).simplify)
+      case Ap(Pure(f), x)       => FMap(x.simplify, f.simplify)
+      case Ap(p, q)             => Ap(p.simplify, q.simplify)
 
-    case Postfix(tpe, p, op)    => Postfix(tpe, p.simplify, op.simplify)
+      case FMap(p, f) => simplifyMap(p, f)
 
-    case LiftN(f, ps, isImplicit) => LiftN(f.simplify, ps.map(_.simplify), isImplicit)
+      // case Many(Empty)      => Empty
+      // case Many(p)          => Many(p.simplify)
 
-    case Pure(f) => Pure(f.simplify)
+      case Postfix(tpe, p, op)    => Postfix(tpe, p.simplify, op.simplify)
 
-    case _ => this
+      // case LiftN(f, ps, isImplicit) => LiftN(f.simplify, ps.map(_.simplify), isImplicit)
+
+      case Pure(f) => Pure(f.simplify)
+
+      case _ => this
+    }
   }
 }
 object Parser {
-  final case class NonTerminal(val ref: Symbol)(implicit doc: SemanticDocument) extends Parser {
+  final case class NonTerminal[A](val ref: Symbol)(implicit doc: SemanticDocument) extends Parser[A] {
     val term = Term.Name(ref.info.get.displayName) // TODO: I think this is correct, but needs checking
   }
 
-  final case class Pure(x: Func) extends Parser {
+  final case class Pure[A](x: Func[A]) extends Parser[A] {
     val term = q"pure(${x.term})"
   }
 
@@ -52,7 +59,7 @@ object Parser {
     val term = Term.Name("empty")
   }
 
-  final case class Choice(p: Parser, q: Parser) extends Parser {
+  final case class Choice[A](p: Parser[A], q: Parser[A]) extends Parser[A] {
     // TODO: think about this
     // Note: this doesn't preserve the original combinator choice (i.e. <|> or |) but it doesn't really matter imo
     val term = q"${p.term} | ${q.term}"
@@ -60,15 +67,15 @@ object Parser {
 
   // TODO: generalise to lift2?
   // final case class Ap[A, B](p: Parser, q: Parser) extends Parser {
-  final case class Ap(p: Parser, q: Parser) extends Parser {
+  final case class Ap[A, B](p: Parser[A => B], q: Parser[A]) extends Parser[B] {
     val term = q"${p.term} <*> ${q.term}"
   }
 
-  final case class FMap(p: Parser, f: Func) extends Parser {
+  final case class FMap[A, B](p: Parser[A], f: Func[A => B]) extends Parser[B] {
     val term = q"${p.term}.map(${f.term})"
   }
 
-  final case class Many(p: Parser) extends Parser {
+  final case class Many[A](p: Parser[A]) extends Parser[List[A]] {
     val term = q"many(${p.term})"
   }
 
@@ -76,26 +83,43 @@ object Parser {
     val term = q"string($s)"
   }
 
-  final case class Postfix(tpe: Type.Name, p: Parser, op: Parser) extends Parser {
+  final case class Postfix[A](tpe: Type.Name, p: Parser[A], op: Parser[A => A]) extends Parser[A] {
     val term = q"chain.postfix[$tpe](${p.term})(${op.term})"
   }
 
-  final case class LiftN(f: Func, ps: List[Parser], isImplicit: Boolean) extends Parser {
-    val n = ps.length
-
+  sealed trait LiftN[A] extends Parser[A]
+  final case class Lift2[A, B, R](f: Func[A => B => R], p1: Parser[A], p2: Parser[B], isImplicit: Boolean) extends LiftN[R] {
     val term = if (isImplicit) {
-      q"${f.term}.lift(..${ps.map(_.term)})"
+      q"${f.term}.lift(${p1.term}, ${p2.term})"
     } else {
-      val lift = Term.Name(s"lift$n")
-      q"$lift(${f.term}, ..${ps.map(_.term)})"
+      q"lift2(${f.term}, ${p1.term}, ${p2.term})"
     }
   }
+  final case class Lift3[A, B, C, R](f: Func[A => B => C => R], p1: Parser[A], p2: Parser[B], p3: Parser[C], isImplicit: Boolean) extends LiftN[R] {
+    val term = if (isImplicit) {
+      q"${f.term}.lift(${p1.term}, ${p2.term}, ${p3.term})"
+    } else {
+      q"lift3(${f.term}, ${p1.term}, ${p2.term}, ${p3.term})"
+    }
+  }
+
+  // TODO: this is 1. homogeneous 2. really wrong lmao
+  // final case class LiftN[A, B](f: Func[A => B], ps: List[Parser[A]], isImplicit: Boolean) extends Parser[B] {
+  //   val n = ps.length
+
+  //   val term = if (isImplicit) {
+  //     q"${f.term}.lift(..${ps.map(_.term)})"
+  //   } else {
+  //     val lift = Term.Name(s"lift$n")
+  //     q"$lift(${f.term}, ..${ps.map(_.term)})"
+  //   }
+  // }
 
   final case class Unknown(unrecognisedTerm: Term) extends Parser {
     val term = unrecognisedTerm
   }
 
-  def apply(term: Term)(implicit env: Map[Symbol, NonTerminalTree], doc: SemanticDocument): Parser = term match {
+  def apply[A](term: Term)(implicit env: Map[Symbol, NonTerminalTree], doc: SemanticDocument): Parser[A] = term match {
     // See https://scalacenter.github.io/scalafix/docs/developers/symbol-matcher.html#unapplytree for how to mitigate
     // against matching multiple times using SymbolMatchers
 
@@ -118,20 +142,32 @@ object Parser {
     case Term.ApplyInfix.After_4_6_0(p, Matchers.<*>(_), _, Term.ArgClause(List(q), _)) => Ap(apply(p), apply(q))
 
     // "liftN(f, p1, ..., pN)"
-    case Term.Apply.After_4_6_0(Matchers.liftExplicit(_), Term.ArgClause(f :: ps, _)) =>
-      LiftN(Opaque(f, shouldCurry = ps.length > 1), ps.map(apply).toList, isImplicit = false)
+    case Term.Apply.After_4_6_0(Matchers.liftExplicit(_), Term.ArgClause(f :: ps, _)) if (ps.length == 2) => {
+      Lift2(Opaque(f, shouldCurry = true), apply(ps(0)), apply(ps(1)), isImplicit = false)
+    }
+    case Term.Apply.After_4_6_0(Matchers.liftExplicit(_), Term.ArgClause(f :: ps, _)) if (ps.length == 3) => {
+      Lift3(Opaque(f, shouldCurry = true), apply(ps(0)), apply(ps(1)), apply(ps(2)), isImplicit = false)
+    }
+
     // "f.lift(p1, ..., pN)"
-    case Term.Apply.After_4_6_0(Term.Select(f, Matchers.liftImplicit(_)), Term.ArgClause(ps, _)) =>
-      LiftN(Opaque(f, shouldCurry = ps.length > 1), ps.map(apply).toList, isImplicit = true)
+    case Term.Apply.After_4_6_0(Term.Select(f, Matchers.liftImplicit(_)), Term.ArgClause(ps, _)) if (ps.length == 2) =>
+      Lift2(Opaque(f, shouldCurry = true), apply(ps(0)), apply(ps(1)), isImplicit = true)
+    case Term.Apply.After_4_6_0(Term.Select(f, Matchers.liftImplicit(_)), Term.ArgClause(ps, _)) if (ps.length == 3) =>
+      Lift3(Opaque(f, shouldCurry = true), apply(ps(0)), apply(ps(1)), apply(ps(2)), isImplicit = true)
 
     // TODO: pattern match on Apply, ApplyInfix so we can still try to find parsers within the term?
     case unrecognisedTerm => Unknown(unrecognisedTerm)
   }
 
-  implicit class ParserOps(p: Parser) {
-    def <|>(q: Parser): Parser = Choice(p, q).simplify
-    def <*> (q: Parser): Parser = Ap(p, q).simplify
-    def map(f: Func): Parser = FMap(p, f).simplify
+  implicit class ParserOps[A](p: Parser[A]) {
+    def <|>(q: Parser[A]): Parser[A] = Choice(p, q).simplify
+  }
+  implicit class ParserOps2[A, B](p: Parser[A => B]) {
+    def <*>(q: Parser[A]): Parser[B] = Ap(p, q).simplify
+  }
+  implicit class ParserOps3[A, B](p: Parser[A]) {
+    def map(f: Func[A => B]): Parser[B] = FMap(p, f).simplify
+  }
 
     // def <|>(q: Parser): Parser = (p, q) match {
     //   case (p, Empty)   => p
@@ -151,5 +187,4 @@ object Parser {
     //   case (p, Id(_)) => p
     //   case (p, f)     => FMap(p, f)
     // }
-  }
 }
