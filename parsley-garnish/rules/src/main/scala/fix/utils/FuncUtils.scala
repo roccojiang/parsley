68580d69e90f04a72b2d5cd90f026045de85bb93
Rocco Jiang <rocco@roccojiang.com>
2024-05-05 18:20:26 +0100

    Label function args as concrete or parameters

--- parsley-garnish/rules/src/main/scala/fix/utils/FuncUtils.scala
+++ parsley-garnish/rules/src/main/scala/fix/utils/FuncUtils.scala
@@ -4,6 +4,10 @@ import scala.meta._
 import scalafix.v1._
 
 object FuncUtils {
+  sealed abstract class FuncArg extends Product with Serializable
+  case class ParameterArg(name: Term.Name) extends FuncArg
+  case class ConcreteArg(arg: Term) extends FuncArg
+
   def extractParamLists(f: Term): List[List[Term.Name]] = f match {
     case Term.Function.After_4_6_0(params, body) => {
       params.values.collect { case Term.Param(_, name: Term.Name, _, _) => name } :: extractParamLists(body)
@@ -19,5 +23,22 @@ object FuncUtils {
     }
 
     recurse(t).reverse
+  }
+
+  def labelFuncArgs(f: Term.Function): List[List[FuncArg]] = {
+    @annotation.tailrec
+    def getFuncBody(f: Term): Term = f match {
+      case Term.Function.After_4_6_0(_, body) => getFuncBody(body)
+      case _ => f
+    }
+
+    val paramNames = extractParamLists(f).flatten.map(_.value).toSet
+    val args = extractArgs(getFuncBody(f))
+
+    args.map(_.map(_ match {
+      // directly compare with string values, since meta.Trees use reference equality
+      case arg: Term.Name if paramNames.contains(arg.value) => ParameterArg(arg)
+      case arg => ConcreteArg(arg)
+    }))
   }
 }
--- parsley-garnish/tests/src/test/scala/fix/FuncUtilsTest.scala
+++ parsley-garnish/tests/src/test/scala/fix/FuncUtilsTest.scala
@@ -10,20 +10,29 @@ import scala.meta._
 import scala.meta.contrib._
 import scala.reflect.ClassTag
 
-import utils.FuncUtils
+import utils.FuncUtils._
 
 class FuncUtilsTest extends AnyFlatSpec with Matchers {
-  implicit def termNameEq[T <: Term : ClassTag] = new Equality[T] {
+  implicit def termEq[T <: Term : ClassTag] = new Equality[T] {
     def areEqual(a: T, b: Any): Boolean = b match {
       case t: T => a.isEqual(t)
       case _ => false
     }
   }
 
+  // Custom equality objects are not applied recursively, so this has to be defined manually
+  // see https://github.com/scalatest/scalatest/issues/917
+  implicit val funcArgEq = new Equality[FuncArg] {
+    def areEqual(a: FuncArg, b: Any): Boolean = a match {
+      case ParameterArg(name) => b.isInstanceOf[ParameterArg] && name.isEqual(b.asInstanceOf[ParameterArg].name)
+      case ConcreteArg(arg) => b.isInstanceOf[ConcreteArg] && arg.isEqual(b.asInstanceOf[ConcreteArg].arg)
+    }
+  }
+
   "extractParamLists" should "return a method's parameter lists" in {
     val term = q"(x, y) => z => x + y + z"
 
-    val actual = FuncUtils.extractParamLists(term)
+    val actual = extractParamLists(term)
     val expected = List(List(Term.Name("x"), Term.Name("y")), List(Term.Name("z")))
 
     assertNestedListsEqual(actual, expected)
@@ -32,7 +41,7 @@ class FuncUtilsTest extends AnyFlatSpec with Matchers {
   "extractArgs" should "return a method call's arguments" in {
     val term = q"f(x, y)(z)"
 
-    val actual = FuncUtils.extractArgs(term)
+    val actual = extractArgs(term)
     val expected = List(List(Term.Name("x"), Term.Name("y")), List(Term.Name("z")))
 
     assertNestedListsEqual(actual, expected)
@@ -41,8 +50,17 @@ class FuncUtilsTest extends AnyFlatSpec with Matchers {
   it should "handle more complex arguments" in {
     val term = q"f(g(x, y), h(z))"
 
-    val actual = FuncUtils.extractArgs(term)
+    val actual = extractArgs(term)
     val expected = List(List(q"g(x, y)", q"h(z)"))
+
+    assertNestedListsEqual(actual, expected)
+  }
+
+  "labelFuncArgs" should "label each method argument as a parameter or concrete value" in {
+    val term = q"(a, b) => c => f(a, 3)(b)(2, c)"
+
+    val actual = labelFuncArgs(term)
+    val expected = List(List(ParameterArg(Term.Name("a")), ConcreteArg(q"3")), List(ParameterArg(Term.Name("b"))), List(ConcreteArg(q"2"), ParameterArg(Term.Name("c"))))
 
     assertNestedListsEqual(actual, expected)
   }
