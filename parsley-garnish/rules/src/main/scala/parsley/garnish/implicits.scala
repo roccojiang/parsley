42b76d4c433685778d810255266f0308b8da9651
Rocco Jiang <rocco@roccojiang.com>
2024-05-25 16:57:01 +0100

    Use scalameta isEqual helper instead of structural comparison

--- parsley-garnish/rules/src/main/scala/parsley/garnish/implicits.scala
+++ parsley-garnish/rules/src/main/scala/parsley/garnish/implicits.scala
@@ -1,18 +1,17 @@
 package parsley.garnish
 
 import scala.meta._
+import scala.meta.contrib._
 import scalafix.v1._
 
 object implicits {
   implicit class TreeOps(private val tree: Tree) extends AnyVal {
     def containsAnyOf(terms: Term*): Boolean = tree.collect {
-      case t: Term if terms.toSeq.exists(term => t.structure == term.structure) => t
+      case t: Term if terms.toSeq.exists(term => t.isEqual(term)) => t
     }.nonEmpty
 
-    def isWithinScope(scope: Tree): Boolean = {
-      tree.structure == scope.structure ||
-        tree.parent.exists(_.isWithinScope(scope))
-    }
+    def isWithinScope(scope: Tree): Boolean =
+      tree.isEqual(scope) || tree.parent.exists(_.isWithinScope(scope))
   }
 
   implicit class TermOps(private val term: Term) extends AnyVal {
